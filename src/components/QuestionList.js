51944a771371eaae0ba52018278464483b76ae82
Chongfong <wl00796865@gmail.com>
2022-12-04 21:21:30 +0800

    feat: style question lists

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,5 +1,5 @@
 import api from '../utils/api';
-import { useEffect } from 'react';
+import { Fragment, useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../app/hooks';
 import { setLoading, setErrorMessage, setQuestions } from './stackoverflow/stackoverflow.slice';
 import Loader from './Loader';
@@ -34,37 +34,38 @@ const QuestionList = () => {
       {currentQuestion.length > 0 ? (
         currentQuestion.map((question) => (
           <div
+            className="flex flex-nowrap py-4 border-b-2 border-sky-200"
             key={question.question_id}
             onClick={(e) => {
               e.stopPropagation;
               window.open(`${question.link}`, '_blank');
             }}
           >
-            <div>
-              <div>
+            <div className="w-4/5 flex flex-wrap">
+              <div className="w-full">
                 <div>{question.title}</div>
               </div>
-              <div>
-                <div>
+              <div className="w-full justify-evenly flex flex-nowrap">
+                <div className="w-1/3">
                   <p>Score</p>
                   <div>{question.score}</div>
                 </div>
-                <div>
+                <div className="w-1/3">
                   <p>Answers</p>
                   <div>{question.answer_count}</div>
                   <div>{question.is_answered === true ? 'ture' : 'false'}</div>
                 </div>
-                <div>
+                <div className="w-1/3">
                   <p>Viewed</p>
                   <div>{question.view_count}</div>
                 </div>
               </div>
             </div>
-            <div>
-              <div>
-                <img src={question.owner.profile_image} />
+            <div className="w-1/5 flex flex-wrap text-center">
+              <div className="w-full">
+                <img src={question.owner.profile_image} className="rounded-full w-20 h-20 mx-auto" />
               </div>
-              <div>{question.owner.display_name}</div>
+              <div className="w-full">{question.owner.display_name}</div>
             </div>
           </div>
         ))
